f841a2f30b3e893bb4130eb69f37846d2c9ab362
steventbradford <82245418+steventbradford@users.noreply.github.com>
2023-09-21 15:21:44 -0400

    Update main.py
    added comments to the code so we have an outline on what to do.

--- main.py
+++ main.py
@@ -1,1 +1,44 @@
+#Lab 3 Python project 
+#Steven Bradford and Asher Applegate
+
+#methods
+
+#method 1: createFiles
+#This method will create a number of files if they do not exist
+#It will accept the parameters fileNamePrefix:str and numOfFiles:int
+
+#method 2: getType
+#This method will return the string "File" if it fileOrDirectoryPath is a file and Directory otherwise
+#It will accept the parameters fileOrDirectoryPath:str
+
+#method 3: renameFile
+#This method will rename a file to the new name
+#It will accept the parameters filename:str and newName:str
+
+#method 4: createDir
+#This method will create a directory if it does not already exist
+#It will accept the parameters nameOfDirectory:str
+
+#method 5: createSubDirecties
+#This method will create numberToCreate subdirectories of the directory directoryName
+#It will accept the parameters directoryName:str and numberToCreate:int
+
+#method 6: renameFiles
+#This method will rename all the files in the target directory with extension currentExt to extenstion newExt.
+#It will accept the parameters targetDirectory:str and currentExt:str and newExt:str
+
+#method 7: displayContents
+#this method will display the list of files and directories of the directory. This should display the following table
+# Name                             Type                
+# -----                           --------
+#<Name of file or directory       <File or Directory>
+#It will accept the parameters directoryName:str
+
+#main method
+#this method will accept no parameters and should implement the following task
+
+
+
+
+
 
