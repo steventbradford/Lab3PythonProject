f7b0bcb2ddea243aeee85787b6ff1a33f412599b
asher <75417601+yreu@users.noreply.github.com>
2023-09-27 19:21:09 -0400

    Update lab3.py
    changed comments

--- lab3.py
+++ lab3.py
@@ -1,5 +1,5 @@
-#Lab 3 Python Project 
-#Steven Bradford and Asher Applegate
+# Lab 3 Python Project 
+# Steven Bradford and Asher Applegate
 
 import os
 
@@ -18,11 +18,9 @@ def validate_positive_integer(input_str):
         pass
     return False, None
 
-#methods
-
-#method 1: createFiles
-#This method will create a number of files if they do not exist
-#It will accept the parameters fileNamePrefix:str and numOfFiles:int
+# Method 1: createFiles
+# This method will create a number of files if they do not exist
+# It will accept the parameters fileNamePrefix:str and numOfFiles:int
 def createFiles(fileNamePrefix:str, numOfFiles:int, extension:str):
     for i in range(1, numOfFiles + 1):
         filename = f"{fileNamePrefix}_{i}.{extension}"
@@ -33,9 +31,9 @@ def createFiles(fileNamePrefix:str, numOfFiles:int, extension:str):
         else:
             print(f"File '{filename}' already exists.")
 
-#method 2: getType
-#This method will return the string "File" if it fileOrDirectoryPath is a file and Directory otherwise
-#It will accept the parameters fileOrDirectoryPath:str
+# Method 2: getType
+# This method will return the string "File" if it fileOrDirectoryPath is a file and Directory otherwise
+# It will accept the parameters fileOrDirectoryPath:str
 def getType(fileOrDirectoryPath:str):
   if os.path.isfile(fileOrDirectoryPath):
     return "File"
@@ -44,9 +42,9 @@ def getType(fileOrDirectoryPath:str):
   else:
     return "Does not exist"
 
-#method 3: renameFile
-#This method will rename a file to the new name
-#It will accept the parameters filename:str and newName:str
+# Method 3: renameFile
+# This method will rename a file to the new name
+# It will accept the parameters filename:str and newName:str
 def renameFile(filename:str, newName:str):
   if os.path.exists(filename):
     new_filename = os.path.join(os.path.dirname(filename), newName)
@@ -55,9 +53,9 @@ def renameFile(filename:str, newName:str):
   else:
     print(f"File '{filename}' does not exist.")
 
-#method 4: createDir
-#This method will create a directory if it does not already exist
-#It will accept the parameters nameOfDirectory:str
+# Method 4: createDir
+# This method will create a directory if it does not already exist
+# It will accept the parameters nameOfDirectory:str
 def createDir(nameOfDirectory:str):
   if not os.path.exists(nameOfDirectory):
     os.mkdir(nameOfDirectory)
@@ -65,9 +63,9 @@ def createDir(nameOfDirectory:str):
   else:
     print (f"Directory '{nameOfDirectory}' already exists.")
 
-#method 5: createSubDirecties
-#This method will create numberToCreate subdirectories of the directory directoryName
-#It will accept the parameters directoryName:str and numberToCreate:int
+# Method 5: createSubDirecties
+# This method will create numberToCreate subdirectories of the directory directoryName
+# It will accept the parameters directoryName:str and numberToCreate:int
 def createSubDirectories(directoryName:str, numberToCreate:int):
   for i in range(1, numberToCreate +1):
     subdirectory = os.path.join(directoryName, f"subdir{i}")
@@ -77,9 +75,9 @@ def createSubDirectories(directoryName:str, numberToCreate:int):
     else:
       print(f"Subdirectory '{subdirectory}' already exists.")
 
-#method 6: renameFiles
-#This method will rename all the files in the target directory with extension currentExt to extenstion newExt.
-#It will accept the parameters targetDirectory:str and currentExt:str and newExt:str
+# Method 6: renameFiles
+# This method will rename all the files in the target directory with extension currentExt to extenstion newExt.
+# It will accept the parameters targetDirectory:str and currentExt:str and newExt:str
 def renameFiles(targetDirectory:str, currentExt:str, newExt:str):
     for root, _, files in os.walk(targetDirectory):
         for filename in files:
@@ -89,9 +87,9 @@ def renameFiles(targetDirectory:str, currentExt:str, newExt:str):
                 os.rename(old_path, new_path)
                 print(f"File '{filename}' renamed to '{new_path}'.")
 
-#method 7: displayContents
-#This method will display the list of files and directories of the directory. This should display the following table
-#It will accept the parameters directoryName:str
+# Method 7: displayContents
+# This method will display the list of files and directories of the directory. This should display the following table
+# It will accept the parameters directoryName:str
 def displayContents(directoryName:str):
   print("Name".ljust(50), "Type")
   print("-" * 50, "------")
@@ -100,8 +98,7 @@ def displayContents(directoryName:str):
     item_type = getType(item_path)
     print(f"{item.ljust(50)} {item_type}")
 
-#main method
-#This method will accept no parameters and should implement the following task
+# Main method
 def main():
 
     # a. Print the name of the current directory
