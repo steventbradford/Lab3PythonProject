c23ebe03ca22c785ecc139c35da3fc71b0e844da
steventbradford <82245418+steventbradford@users.noreply.github.com>
2023-10-03 14:00:39 -0400

    Update Lab4.py
    added the function for reading the file and getting the data.

--- Lab4.py
+++ Lab4.py
@@ -1,1 +1,15 @@
+import csv
 
+#Write a Python script that includes at least three user-defined functions; one for reading the data from
+#the given file, another for creating the new file with suspicious login employee information, and the third
+#for calling the other two functions
+
+#function for reading the data in the csv file
+def readEmployeeData(file_path):
+  employee_data = []
+  with open(file_path, 'r') as file:
+    csv_reader = csv.reader(file)
+    header = next(csv_reader) #This skips the header
+    for row in csv_reader:
+      employee_data.append(row)
+    return employee_data
