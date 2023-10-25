a310c422f7b08820a2964710bd3f96d75f1b56b7
asher <75417601+yreu@users.noreply.github.com>
2023-10-25 18:18:09 -0400

    Update lab6.py

--- lab6.py
+++ lab6.py
@@ -1,10 +1,16 @@
+#!/usr/bin/python3
+# Group 17 - Asher Applegate and Steven Bradford
+# Lab 6
+
+# Remember to launch PyCharm through root!
+
 import os
 import pwd
 import crypt
 import random
 import string
 
-# Function to check if a username exists in the system
+# Function to check if the username exists
 def username_exists(username):
     try:
         pwd.getpwnam(username)
@@ -12,7 +18,7 @@ def username_exists(username):
     except KeyError:
         return False
 
-# Function to generate a random password
+# Function to generate a random password; 8 characters - combination of letters, numbers, and symbols
 def generate_password(length=8):
     characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(characters) for i in range(length))
@@ -24,11 +30,12 @@ def create_user():
     if username_exists(username):
         print("User already exists.")
         return False
-    
     full_name = input("Enter full name: ")
+
+    # Password applied and encrypted with SHA256
     password = generate_password()
     encrypted_password = crypt.crypt(password, crypt.METHOD_SHA256)
-    
+
     try:
         os.system(f"useradd -m -p {encrypted_password} -c '{full_name}' {username}")
         print(f"User '{username}' created successfully with password: {password}")
@@ -59,10 +66,11 @@ def modify_user():
         print("User does not exist.")
         return False
     
+    # Modification menu function
     print("Choose an option:")
     print("1. Lock the account")
-    print("2. Change the real name associated with the account")
-    choice = input("Enter your choice: ")
+    print("2. Change the full name of the account")
+    choice = input("Enter choice: ")
     
     if choice == "1":
         os.system(f"passwd -l {username}")
@@ -85,12 +93,12 @@ def modify_user():
 def main_menu():
     while True:
         print("User Account Management Menu:")
-        print("1. Check if username exists")
+        print("1. Check if a username exists")
         print("2. Create a new user")
         print("3. Remove a user")
         print("4. Modify a user account")
         print("5. Exit")
-        choice = input("Enter your choice: ")
+        choice = input("Enter choice: ")
 
         if choice == "1":
             username = input("Enter username to check: ")
@@ -105,11 +113,11 @@ def main_menu():
         elif choice == "4":
             modify_user()
         elif choice == "5":
-            print("Exiting. Goodbye!")
+            print("Exiting.")
             break
         else:
-            print("Invalid choice. Please try again.")
+            print("Invalid choice.")
 
-# Entry point of the script
+# Main
 if __name__ == "__main__":
     main_menu()
