6f2e9387f4f313889aa4041bf122a1447389430b
asher <75417601+yreu@users.noreply.github.com>
2023-12-13 15:56:32 -0500

    Update finalpart2test.py

--- finalpart2test.py
+++ finalpart2test.py
@@ -1,8 +1,32 @@
+#!/usr/bin/python
+# Group 17
+# Asher Applegate and Steven Bradford
+
+# This script creates users and groups based on the names of the employees and the groups in the defined file. 
+# The usernames will be created based off the syntax “[last name][first letter of first name][N]” with N being either blank or a value, starting with 1 and increasing by 1 for each duplicate username, depending on if a user with that username already exists.  
+# The users will be added to the corresponding groups as is defined in the employee file. 
+# The First Name, Last Name, Username, and Password generated will be written to the CSV file that is defined. 
+
+# Example command: python ./final_proj_17_part2.py /home/student/employee-f23.csv /home/student/useraccounts.csv
+
 import csv
 import subprocess
 import os
 import argparse
 import time
+
+def create_group(group):
+    # Check if the group already exists
+    try:
+        subprocess.run(["getent", "group", group], check=True, stdout=subprocess.PIPE)
+        print(f"Group already exists: {group}")
+    except subprocess.CalledProcessError:
+        # Group does not exist, create it
+        try:
+            subprocess.run(["sudo", "groupadd", group])
+            print(f"Group created: {group}")
+        except subprocess.CalledProcessError as e:
+            print(f"Error creating group {group}: {e}")
 
 def create_user_account(username, full_name, group):
     # Create a user account using the Linux useradd command
@@ -14,8 +38,11 @@ def create_user_account(username, full_name, group):
 
 def check_duplicate_user(username):
     # Check if the username already exists in /etc/passwd
-    with open('/etc/passwd', 'r') as passwd_file:
-        return any(username == line.split(':')[0] for line in passwd_file)
+    try:
+        subprocess.run(["grep", username, "/etc/passwd"], check=True)
+        return True
+    except subprocess.CalledProcessError:
+        return False
 
 def generate_username(first_name, last_name, existing_usernames):
     # Generate a unique username based on the first and last name
@@ -38,9 +65,12 @@ def process_employee_file(employee_file_path, output_file_path, log_file_path):
     with open(employee_file_path, 'r') as file:
         reader = csv.DictReader(file)
         for row in reader:
-            first_name = row['First Name']
-            last_name = row['Last Name']
-            group = row['Group']
+            first_name = row['first_name']
+            last_name = row['last_name']
+            group = row['user_group']
+
+            # Create user group if it does not exist
+            create_group(group)
 
             # Generate a unique username
             username = generate_username(first_name, last_name, existing_usernames)
@@ -59,7 +89,7 @@ def process_employee_file(employee_file_path, output_file_path, log_file_path):
         writer = csv.writer(output_file)
         writer.writerow(['First Name', 'Last Name', 'Username'])
         for username in existing_usernames:
-            full_name = subprocess.check_output(["grep", username, "/etc/passwd"]).decode().split(":")[4]
+            full_name = subprocess.check_output(["grep", username, "/etc/passwd"], stderr=subprocess.DEVNULL).decode().split(":")[4]
             first_name, last_name = full_name.strip().split(' ', 1)
             writer.writerow([first_name, last_name, username])
 
@@ -76,7 +106,7 @@ def main():
     parser.add_argument("E_FILE_PATH", help="The path to the employee file.")
     parser.add_argument("OUTPUT_FILE_PATH", help="The path to the output file.")
     parser.add_argument("-l", "--log", dest="LOG_FILE_PATH", help="Log file name.")
-    parser.add_argument("-h", "--help", action="help", help="Show this help message and exit.")
+    parser.add_argument("-H", "--Help", action="help", help="Show this help message and exit.")
 
     args = parser.parse_args()
 
