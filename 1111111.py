d5e29b540bf14728a9acf1f0fef46de46ad5a07c
asher <75417601+yreu@users.noreply.github.com>
2023-12-13 16:28:31 -0500

    Update 1111111.py

--- 1111111.py
+++ 1111111.py
@@ -2,68 +2,35 @@
 # Group 17
 # Asher Applegate and Steven Bradford
 
-# This script creates users and groups based on the names of the employees and the groups in the defined file. 
-# The usernames will be created based on the syntax “[last name][first letter of first name][N]” with N being either blank or a value, starting with 1 and increasing by 1 for each duplicate username, depending on if a user with that username already exists.  
-# The users will be added to the corresponding groups as defined in the employee file. 
-# The First Name, Last Name, Username, and Password generated will be written to the CSV file that is defined. 
+# This script deletes users and groups based on the names of the employees and the groups in the defined file. 
+# The usernames are created based on the syntax “[last name][first letter of first name][N]” with N being either blank or a value, starting with 1 and increasing by 1 for each duplicate username, depending on if a user with that username already exists.  
+# The script deletes the corresponding users and groups as defined in the employee file. 
 
-# Example command: python ./final_proj_17_part2.py /home/student/employee-f23.csv /home/student/useraccounts.csv
+# Example command: python ./final_proj_17_part2.py /home/student/employee-f23.csv
 
 import csv
 import subprocess
-import os
 import argparse
 import time
 
-def create_group(group):
-    # Check if the group already exists
+def delete_user_account(username):
+    # Delete a user account using the Linux userdel command
     try:
-        subprocess.run(["getent", "group", group], check=True, stdout=subprocess.PIPE)
-        print(f"Group already exists: {group}")
-    except subprocess.CalledProcessError:
-        # Group does not exist, create it
-        try:
-            subprocess.run(["sudo", "groupadd", group])
-            print(f"Group created: {group}")
-        except subprocess.CalledProcessError as e:
-            print(f"Error creating group {group}: {e}")
-
-def create_user_account(username, full_name, group):
-    # Check for duplicate user accounts
-    if check_duplicate_user(username):
-        print(f"Duplicate user account found: {username}")
-        return
-
-    # Create a user account using the Linux useradd command
-    try:
-        subprocess.run(["sudo", "useradd", "-m", "-c", full_name, "-G", group, username])
-        print(f"User account created: {username}")
+        subprocess.run(["sudo", "userdel", "-r", username])
+        print(f"User account deleted: {username}")
     except subprocess.CalledProcessError as e:
-        print(f"Error creating user account for {username}: {e}")
+        print(f"Error deleting user account for {username}: {e}")
 
-def check_duplicate_user(username):
-    # Check if the username already exists in /etc/passwd
+def delete_group(group):
+    # Delete a group using the Linux groupdel command
     try:
-        subprocess.run(["grep", username, "/etc/passwd"], check=True)
-        return True
-    except subprocess.CalledProcessError:
-        return False
+        subprocess.run(["sudo", "groupdel", group])
+        print(f"Group deleted: {group}")
+    except subprocess.CalledProcessError as e:
+        print(f"Error deleting group {group}: {e}")
 
-def generate_username(first_name, last_name, existing_usernames):
-    # Generate a unique username based on the first and last name
-    base_username = f"{last_name.lower()}{first_name.lower()[0]}"
-    username = base_username
-    suffix = 1
-
-    while username in existing_usernames:
-        username = f"{base_username}{suffix}"
-        suffix += 1
-
-    return username
-
-def process_employee_file(employee_file_path, output_file_path, log_file_path):
-    # Process the employee file and create user accounts
-    existing_usernames = set()
+def process_employee_file(employee_file_path, log_file_path):
+    # Process the employee file and delete user accounts and groups
     log_messages = []
 
     # Read employee details from CSV file
@@ -75,24 +42,25 @@ def process_employee_file(employee_file_path, output_file_path, log_file_path):
             group = row['user_group']
 
             # Generate a unique username
-            username = generate_username(first_name, last_name, existing_usernames)
+            base_username = f"{last_name.lower()}{first_name.lower()[0]}"
+            username = base_username
+            suffix = 1
 
-            # Create user account
-            create_user_account(username, f"{last_name} {first_name}", group)
-            
-            # Create user group if it does not exist
-            create_group(group)
+            while True:
+                try:
+                    subprocess.run(["sudo", "userdel", "-r", username], check=True)
+                    print(f"User account deleted: {username}")
+                except subprocess.CalledProcessError:
+                    break
 
-            existing_usernames.add(username)
+                username = f"{base_username}{suffix}"
+                suffix += 1
 
-    # Write user account details to CSV file
-    with open(output_file_path, 'w', newline='') as output_file:
-        writer = csv.writer(output_file)
-        writer.writerow(['First Name', 'Last Name', 'Username'])
-        for username in existing_usernames:
-            full_name = subprocess.check_output(["grep", username, "/etc/passwd"], stderr=subprocess.DEVNULL).decode().split(":")[4]
-            first_name, last_name = full_name.strip().split(' ', 1)
-            writer.writerow([first_name, last_name, username])
+            try:
+                subprocess.run(["sudo", "groupdel", group], check=True)
+                print(f"Group deleted: {group}")
+            except subprocess.CalledProcessError:
+                pass
 
     # Write log messages to log file
     if log_file_path:
@@ -103,16 +71,15 @@ def process_employee_file(employee_file_path, output_file_path, log_file_path):
                 log_file.write(f"{timestamp}: {message}\n")
 
 def main():
-    parser = argparse.ArgumentParser(description="Create user accounts based on employee details.")
+    parser = argparse.ArgumentParser(description="Delete user accounts and groups based on employee details.")
     parser.add_argument("E_FILE_PATH", help="The path to the employee file.")
-    parser.add_argument("OUTPUT_FILE_PATH", help="The path to the output file.")
     parser.add_argument("-l", "--log", dest="LOG_FILE_PATH", help="Log file name.")
     parser.add_argument("-H", "--Help", action="help", help="Show this help message and exit.")
 
     args = parser.parse_args()
 
-    # Process the employee file and create user accounts
-    process_employee_file(args.E_FILE_PATH, args.OUTPUT_FILE_PATH, args.LOG_FILE_PATH)
+    # Process the employee file and delete user accounts and groups
+    process_employee_file(args.E_FILE_PATH, args.LOG_FILE_PATH)
 
 if __name__ == "__main__":
     main()
