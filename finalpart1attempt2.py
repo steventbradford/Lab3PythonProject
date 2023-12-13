3cf5ecb9b3372b38eaf3c3804c0845a9598bf3a5
asher <75417601+yreu@users.noreply.github.com>
2023-12-13 12:12:32 -0500

    Update finalpart1attempt2.py

--- finalpart1attempt2.py
+++ finalpart1attempt2.py
@@ -1,114 +1,110 @@
 import os
-import sys
 import paramiko
 import smtplib
-from email.mime.multipart import MIMEMultipart
+import argparse
 from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from email import encoders
-from datetime import datetime, timedelta
-import getpass
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+from getpass import getpass
 
-class FileMonitor:
-    def __init__(self, ip_address, username, password, recipient_email, download_path=None, display=False):
-        self.ip_address = ip_address
-        self.username = username
-        self.password = password
-        self.recipient_email = recipient_email
-        self.download_path = download_path
-        self.display = display
+def connect_to_server(ip_address, username, password):
+    # Method to establish an SSH connection to the target server
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        ssh_client.connect(ip_address, username=username, password=password)
+        return ssh_client
+    except Exception as e:
+        print(f"Error connecting to the server: {e}")
+        return None
 
-    def connect_to_server(self):
-        # Connect to the target server using SSH
-        try:
-            client = paramiko.SSHClient()
-            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            client.connect(self.ip_address, username=self.username, password=self.password)
-            return client
-        except Exception as e:
-            print(f"Error connecting to the server: {e}")
-            sys.exit(1)
+def find_affected_files(ssh_client):
+    # Method to identify affected files based on the specified criteria
+    stdin, stdout, stderr = ssh_client.exec_command(
+        "find ~/ -type f -ctime -30 -mtime -7"
+    )
+    files_list = stdout.read().decode().splitlines()
+    return files_list
 
-    def list_affected_files(self, client):
-        # List files in the home directory modified in the last month but created in the past week
-        cmd = 'find ~ -type f -mtime -30 -ctime -7'
-        stdin, stdout, stderr = client.exec_command(cmd)
-        files = stdout.read().decode().splitlines()
-        return files
+def display_files(files_list):
+    # Method to display the contents of affected files if the option is specified
+    for file_path in files_list:
+        print(f"Displaying contents of: {file_path}")
+        stdin, stdout, stderr = ssh_client.exec_command(f"cat {file_path}")
+        print(stdout.read().decode())
 
-    def send_email(self, files):
-        # Compose and send an email with the list of affected files
-        sender_email = "your.email@gmail.com"  # Replace with your actual email address
-        sender_password = "your_email_password"  # Replace with your actual email password
+def send_email(sender_email, sender_password, recipient_email, files_list, affected_user):
+    # Method to send an email with a list of affected files to the CTO
+    subject = "Security Alert - Files Compromised"
+    body = f"The following files in the home directory of user '{affected_user}' have been compromised:\n\n"
+    for file_path in files_list:
+        body += f"{file_path}\n"
+    
+    msg = MIMEMultipart()
+    msg['From'] = sender_email
+    msg['To'] = recipient_email
+    msg['Subject'] = subject
+    msg.attach(MIMEText(body, 'plain'))
 
-        msg = MIMEMultipart()
-        msg['From'] = sender_email
-        msg['To'] = self.recipient_email
-        msg['Subject'] = "File Monitor Alert"
-
-        body = f"The following files have been compromised on {self.ip_address}:\n\n"
-        body += "\n".join(files)
-        msg.attach(MIMEText(body, 'plain'))
-
-        # Attach the smallest file
-        smallest_file = min(files, key=os.path.getsize)
-        attachment = open(smallest_file, 'rb')
-        part = MIMEBase('application', 'octet-stream')
-        part.set_payload((attachment).read())
-        encoders.encode_base64(part)
-        part.add_header('Content-Disposition', f"attachment; filename= {smallest_file}")
+    # Attach the smallest affected file
+    smallest_file = min(files_list, key=lambda x: os.path.getsize(x))
+    with open(smallest_file, 'rb') as attachment:
+        part = MIMEApplication(attachment.read(), Name=os.path.basename(smallest_file))
+        part['Content-Disposition'] = f'attachment; filename="{os.path.basename(smallest_file)}"'
         msg.attach(part)
 
-        # Send email using Gmail SMTP
-        with smtplib.SMTP('smtp.gmail.com', 587) as server:
-            server.starttls()
-            server.login(sender_email, sender_password)
-            server.sendmail(sender_email, self.recipient_email, msg.as_string())
+    # Send the email
+    with smtplib.SMTP('smtp.gmail.com', 587) as server:
+        server.starttls()
+        server.login(sender_email, sender_password)
+        server.sendmail(sender_email, recipient_email, msg.as_string())
 
-    def download_file(self, client, file_path):
-        # Download the smallest affected file to the specified path
-        sftp = client.open_sftp()
-        local_path = os.path.join(self.download_path or os.path.expanduser("~"), os.path.basename(file_path))
-        sftp.get(file_path, local_path)
-        sftp.close()
-        print(f"File downloaded to: {local_path}")
+def download_file(ssh_client, file_path, download_path):
+    # Method to download the smallest affected file to the specified download path
+    sftp = ssh_client.open_sftp()
+    remote_file = file_path
+    local_file = os.path.join(download_path, os.path.basename(file_path))
+    sftp.get(remote_file, local_file)
+    sftp.close()
+    print(f"Downloaded file to: {local_file}")
 
-    def display_files(self, files):
-        # Display the contents of the affected files
-        for file in files:
-            with open(file, 'r') as f:
-                print(f.read())
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Script to monitor files impacted by attacks.")
+    parser.add_argument("IP_ADDRESS", help="The IP address of the compromised computer.")
+    parser.add_argument("USERNAME", help="The username of the user on the affected computer.")
+    parser.add_argument("-d", "--disp", action="store_true", help="Display the contents of affected files.")
+    parser.add_argument("-e", "--email", dest="RECPT_EMAIL", required=True, help="Email address of the CTO.")
+    parser.add_argument("-p", "--path", dest="DOWNLOAD_PATH", help="Download affected files to this path.")
+    parser.add_argument("-H", "--help", action="help", help="Show this help message and exit.")
 
-    def run(self):
-        # Main execution of the script
-        client = self.connect_to_server()
-        affected_files = self.list_affected_files(client)
+    return parser.parse_args()
 
-        if affected_files:
-            print("Affected Files:")
-            for file in affected_files:
-                print(file)
+def main():
+    # Parse command-line arguments
+    args = parse_arguments()
 
-            if self.display:
-                self.display_files(affected_files)
+    # Get user password securely
+    password = getpass(prompt="Enter password: ")
 
-            self.send_email(affected_files)
+    # Connect to the server
+    ssh_client = connect_to_server(args.IP_ADDRESS, args.USERNAME, password)
 
-            if self.download_path:
-                self.download_file(client, min(affected_files, key=os.path.getsize))
+    # Find affected files
+    files_list = find_affected_files(ssh_client)
 
-        else:
-            print("No affected files found.")
+    # Display files if option is specified
+    if args.disp:
+        display_files(files_list)
+
+    # Send email to CTO
+    send_email("CTOCIT383@gmail.com", "CTOCIT383", args.RECPT_EMAIL, files_list, args.USERNAME)
+
+    # Download the smallest affected file
+    download_path = args.DOWNLOAD_PATH if args.DOWNLOAD_PATH else os.path.expanduser("~")
+    download_file(ssh_client, min(files_list, key=lambda x: os.path.getsize(x)), download_path)
+
+    # Close SSH connection
+    ssh_client.close()
 
 if __name__ == "__main__":
-    # Get user inputs
-    ip_address = input("Enter the IP address: ")
-    username = input("Enter the username: ")
-    password = getpass.getpass("Enter the password: ")
-    recipient_email = input("Enter the CTO's email address: ")
-    download_path = input("Enter the download path (leave empty for home directory): ")
-    display_files = '-d' in sys.argv or '--disp' in sys.argv
-
-    # Create an instance of the FileMonitor class and run the script
-    file_monitor = FileMonitor(ip_address, username, password, recipient_email, download_path, display_files)
-    file_monitor.run()
+    main()
